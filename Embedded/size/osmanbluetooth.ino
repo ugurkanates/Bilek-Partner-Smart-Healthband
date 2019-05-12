4c84ed2e3799b228814fea9f330dc3928437ab1d
Ugurkan Ates <uates@gtu.edu.tr>
2019-05-12 21:41:30 +0300

    Create osmanbluetooth.ino

---
+++ Embedded/size/osmanbluetooth.ino
@@ -0,0 +1,19 @@
+
+void setup() {
+  Serial.begin(9600);
+
+  
+}
+
+void loop() {
+  // Server'a nasıl veri gonderiyosanız onu buraya yaz.
+
+   
+   // bluetooth stuff
+        Serial.println("24.5");
+   Serial.println("60.0");
+   //Serial.println(name);
+
+  
+  delay(1000); // this is shit
+}
